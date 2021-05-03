317a0501af29e02dce2c7b31f8a221bbfa0f6946
ThomasMathien <tmathien@excilys.com>
2021-05-03 17:09:10 +0200

    add:dashboard table

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <div id="app">
     <div id="nav">
       <router-link to="/login">Login</router-link> |
-      <router-link to="/dashboard">Dashboard</router-link>
+      <router-link to="/dashboard" >Dashboard</router-link>
     </div>
-    <router-view/>
+    <router-view role="ROLE_ADMIN" />
   </div>
 </template>
 
